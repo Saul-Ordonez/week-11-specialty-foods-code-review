a47ea52a6a87251a3c420a7d232652d8f7233195
Saul Ordonez <sauleordonez@gmail.com>
2020-01-17 10:32:41 -0800

    add create and show controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,29 @@
 class ReviewsController < ApplicationController
+  
   def new
     @product = Product.find(params[:product_id])
     @reviews = @product.reviewss.new
     render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Song.find(params[:id])
+    render :show
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
   end
 end
