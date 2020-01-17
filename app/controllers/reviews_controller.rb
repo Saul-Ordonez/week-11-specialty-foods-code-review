5846d5c9b90d4d8eddc4cc8f0582496af53581b2
Saul Ordonez <sauleordonez@gmail.com>
2020-01-17 10:51:05 -0800

    add update and destroy controller methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,13 +23,29 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-  @product = Product.find(params[:product_id])
-  @review = Review.find(params[:id])
-  render :edit
-end
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
 
   private
   def review_params
     params.require(:review).permit(:author, :content_body, :rating)
   end
+  
 end
