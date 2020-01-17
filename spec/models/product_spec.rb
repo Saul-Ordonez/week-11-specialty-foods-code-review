ac855ac7e8b258b239e0eb7613268c69b07658ef
Saul Ordonez <sauleordonez@gmail.com>
2020-01-17 14:01:40 -0800

    add presence validation specs and pass them

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
 end
+
+  # it { should validate_length_of(:name).is_at_most(100) }
+  # it { should allow_value('tim').for(:name) }
+  # it { should_not allow_value('123').for(:name) }
+  # it { should_not allow_value('~').for(:name) }
