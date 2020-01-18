ae197fd73ac3e53dbb5f771b4a8087b142ea6bfa
Saul Ordonez <sauleordonez@gmail.com>
2020-01-18 12:02:07 -0800

    add scope for top 3 most recent products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   scope :united_states, -> { where(country_of_origin: "United States of America") }
+  scope :most_recent, -> { order(created_at: :desc).first(3) }
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :cost, presence: true
