afdea3e5f04700c9c4b7b6dc1830272eebb9e4db
Saul Ordonez <sauleordonez@gmail.com>
2020-01-24 12:00:20 -0800

    re-add users table to include admin authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_24_183905) do
+ActiveRecord::Schema.define(version: 2020_01_24_194645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_01_24_183905) do
     t.string "email"
     t.string "password_hash"
     t.string "password_salt"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "products"
